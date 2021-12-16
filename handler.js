d8b27c3d149a5719ed3cbaebc251513ec880297d
Dmytro Mishchenko <dmishchenko@vp.software>
2021-12-16 13:14:31 +0200

    Added sqs + dlq conf in serverless.yml

--- handler.js
+++ handler.js
@@ -1,18 +1,23 @@
-'use strict';
+import AWS from 'aws-sdk';
 
-module.exports.hello = async (event) => {
+const sqsClient = new AWS.SQS({
+  region: "eu-central-1"
+})
+
+async function hello(event){
+  console.log(sqsClient);
+  console.log('-----------------');
+  console.log(sqsClient.listQueues());
   return {
     statusCode: 200,
     body: JSON.stringify(
       {
         message: 'Go Serverless v1.0! Your function executed successfully!',
-        input: event,
       },
-      null,
-      2
     ),
   };
-
+}
+  export const handler = hello;
   // Use this code if you don't use the http event with the LAMBDA-PROXY integration
   // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
-};
+
