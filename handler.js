9a9255bd688c836b04f2e7b6ef741fd02762f92d
Dmytro Mishchenko <dmishchenko@vp.software>
2021-12-16 19:54:53 +0200

    Tested CreateQueue Logic

--- handler.js
+++ handler.js
@@ -1,13 +1,30 @@
 import AWS from 'aws-sdk';
+import { SQSClient, CreateQueueCommand, AddPermissionCommand } from '@aws-sdk/client-sqs';
 
-const sqsClient = new AWS.SQS({
+const sqsClient = new SQSClient({
   region: "eu-central-1"
 })
 
 async function hello(event){
-  console.log(sqsClient);
-  console.log('-----------------');
-  console.log(sqsClient.listQueues());
+  console.log('===========');
+  // await sqsClient.listQueues({}, (err, data) =>{
+  //   console.log('Hello');
+  //   if(err){
+  //     console.log(err);
+  //   }else{
+  //     console.log(data);
+  //   }
+  // });
+  // await sqsClient.createQueue({QueueName: 'SqsClient'}, function(err, data) {
+  //   if (err) console.log(err, err.stack); // an error occurred
+  //   else console.log(data);           // successful response
+  // });
+  const command = new CreateQueueCommand({QueueName: "SQSClient"});
+  // const command1 = new AddPermissionCommand();
+  // await sqsClient.send(command1);
+  const data = await sqsClient.send(command);
+  console.log(data);
+  console.log("========");
   return {
     statusCode: 200,
     body: JSON.stringify(
